b5910694d4865c62d83c96f02362765f4d6356e5
saloed <fantom304gmail.com>
2021-04-05 20:24:26 +0300

    second commit

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    image = models.ImageField(upload_to='pictures/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import index
 
 
 urlpatterns = [
+    path('', index),
+    path('index', index)
 ]
